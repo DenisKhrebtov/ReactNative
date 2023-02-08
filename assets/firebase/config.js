fddf0e018706e2adcbce116e8ab5e68122f5d502
DenisKhrebtov <khrebtovdenis@gmail.com>
2023-02-08 03:39:39 +0200

    fix config

--- assets/firebase/config.js
+++ assets/firebase/config.js
@@ -3,14 +3,14 @@ import "firebase/auth";
 import "firebase/storage";
 import "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCD9fEknxuzwPoDPMTPyE39TtakAbk81iM",
-  authDomain: "hw-for-reactnative.firebaseapp.com",
-  projectId: "hw-for-reactnative",
-  storageBucket: "hw-for-reactnative.appspot.com",
-  messagingSenderId: "670235340768",
-  appId: "1:670235340768:web:c914168c4b4bacd2a20585",
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyBFXjfUliNknb2IFkiEto6ecf4MT-1XPZs",
+    authDomain: "hw-2-for-native.firebaseapp.com",
+    projectId: "hw-2-for-native",
+    storageBucket: "hw-2-for-native.appspot.com",
+    messagingSenderId: "247511527146",
+    appId: "1:247511527146:web:22573afc993804586cf193"
+  };
 
 // Initialize Firebase
 export default firebase.initializeApp(firebaseConfig);
